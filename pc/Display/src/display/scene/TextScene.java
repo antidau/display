2296fadda4407a598945d347354de936c2c3729e
Jakob Wenzel <jakobwenzel92@googlemail.com>
2012-07-16 21:35:59 +0200

    added textscene, empty for now

---
+++ pc/Display/src/display/scene/TextScene.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package display.scene;
+
+import display.draw.Image;
+
+/**
+ *
+ * @author wilson
+ */
+public class TextScene extends Scene {
+
+    @Override
+    public String getName() {
+        return "Text";
+    }
+
+    @Override
+    public void drawFrame(Image img, float delta) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
