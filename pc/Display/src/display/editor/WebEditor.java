b427d0c37b7cc660ceed47c63e2402a749dd3b8d
Jakob Wenzel <jakobwenzel92@googlemail.com>
2012-07-16 11:35:52 +0200

    added loading from classpath

--- pc/Display/src/display/editor/WebEditor.java
+++ pc/Display/src/display/editor/WebEditor.java
@@ -6,8 +6,8 @@ package display.editor;
 
 import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import javax.activation.MimetypesFileTypeMap;
 
 /**
@@ -22,7 +22,7 @@ public class WebEditor implements WebRequestHandler {
     public WebEditor() {
         server = new WebServer(this);
         mime.addMimeTypes("text/html html htm");
-        mime.addMimeTypes("image/png png");
+        mime.addMimeTypes("image/png png"); 
     }
 
     public void stop() {
@@ -34,7 +34,8 @@ public class WebEditor implements WebRequestHandler {
     }
 
     protected String escapePath(String path) {
-        return "/home/wilson/tlc5940/pc/Display/src/display/editor/webPages" + path;
+        if (path.contains("..")) return null;
+        return "/display/editor/webPages" + path;
     }
 
     protected void readFile(String path, DataOutputStream output,
@@ -42,25 +43,30 @@ public class WebEditor implements WebRequestHandler {
         try {
 
             String p = escapePath(path);
-            FileInputStream requestedfile = new FileInputStream(p);
+            InputStream requestedfile = this.getClass()
+                                .getResourceAsStream(p);
             String file_type = mime.getContentType(new File(p.toLowerCase()));
 
-            output.writeBytes(WebServer.httpHeader(200, file_type));
-            if (printBody) {
-                byte[] buf = new byte[1024];
-                int len;
-                while ((len = requestedfile.read(buf)) > 0) {
-                    output.write(buf, 0, len);
+            if (requestedfile!=null) {
+                output.writeBytes(WebServer.httpHeader(200, file_type));
+                if (printBody) {
+                    byte[] buf = new byte[1024];
+                    int len;
+                    while ((len = requestedfile.read(buf)) > 0) {
+                        output.write(buf, 0, len);
+                    }
                 }
-            }
-            requestedfile.close();
-        } catch (Exception e) {
-            try {
-                //if you could not open the file send a 404
+                requestedfile.close();
+            } else {
                 output.writeBytes(WebServer.httpHeader(404));
                 output.writeBytes("not found");
-                //close the stream
-                output.close();
+            }
+        } catch (Exception e) {
+            try {
+                e.printStackTrace();
+                //something went wrong, send 500
+                output.writeBytes(WebServer.httpHeader(500));
+                output.writeBytes("error");
             } catch (Exception e2) {
             }
 
@@ -80,6 +86,7 @@ public class WebEditor implements WebRequestHandler {
             output.writeBytes("\r\n");
             System.out.println("redirected");
         } else if (path.startsWith("/json/")) {
+            output.writeBytes(WebServer.httpHeader(200));
             System.out.println("json stuff");
         } else {
             readFile(path, output, printBody);
