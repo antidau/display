0781655f4f86d52aab3e4b2ac33df89edb3868ac
Jakob Wenzel <jakobwenzel92@googlemail.com>
2012-07-19 16:13:34 +0200

    Scenelist funktioniert

--- pc/Display/src/display/editor/webPages/main.js
+++ pc/Display/src/display/editor/webPages/main.js
@@ -1,11 +1,19 @@
 $(function(){
+        
+    sceneList = $('#sceneList');
+    loading = "loading";
 
     function loadSceneList() {
+        sceneList.html(loading);
         $.ajax({
             url: '/json/scene/list',
             dataType: 'json',
             success: function(msg) {
-                alert(msg[1]);
+                sceneList.html('');
+                for (i=0;i<msg.length;i++) {
+                    alert(msg[i]);
+                    sceneList.append('<div class="scene">'+msg[i]+'</div>');
+                }
             },
             fail: function(err) {
                 alert(err);
