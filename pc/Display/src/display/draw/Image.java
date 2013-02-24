5d5201689965d4e9644ffe172f242620f7adce36
Jakob Wenzel <jakobwenzel92@gmail.com>
2013-02-24 15:27:31 +0100

    Made TextScene work

--- pc/Display/src/display/draw/Image.java
+++ pc/Display/src/display/draw/Image.java
@@ -11,9 +11,16 @@ package display.draw;
 public class Image {
     final public static int WIDTH = 64;
     final public static int HEIGHT = 8;
-    //Minimum value =0, Maximum value=1, y first then x
+    /* Displayed image content.
+     * Minimum value =0, Maximum value=1, y first then x
+     */
     public float[][] data;
     public Image() {
         data = new float[HEIGHT][WIDTH];
+    }
+    public void fill(float color) {
+        for (int y=0;y<HEIGHT;y++)
+            for (int x=0;x<WIDTH;x++)
+                data[y][x]=color;
     }
 }
--- pc/Display/src/display/scene/TextScene.java
+++ pc/Display/src/display/scene/TextScene.java
@@ -5,6 +5,7 @@
 package display.scene;
 
 import display.draw.Image;
+import display.scene.text.Font;
 
 /**
  *
@@ -16,10 +17,42 @@ public class TextScene extends Scene {
     public String getName() {
         return "Text";
     }
-
+    
+    String content = "Hello World";
+    Font font = new Font();
+    Character fallback='?';
+    int position=0;
+    float deltasum=0;
+    
+    
     @Override
     public void drawFrame(Image img, float delta) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        deltasum+=delta;
+        img.fill(0);
+        if (deltasum>1f) { position--; deltasum-=1f; System.out.println("hello");}
+        int relposition = position;
+        int currentChar =0;
+        while (relposition<Image.WIDTH && currentChar<content.length()) {
+            char c = content.charAt(currentChar);
+            int[] data = font.getCharacter(c,fallback);
+            
+            if (data!=null) {
+                
+                for (int x =0;x<5;x++) {
+                    if (relposition+x>=Image.WIDTH) break;
+                    if (relposition+x<0) continue;
+                    for (int y=0;y<8;y++) {
+                        int p=(data[x] >> y) & 1;
+                        img.data[y][x+relposition]=p;
+                    }
+                    
+                }
+                relposition+=6;
+                
+            }
+            currentChar++;
+            
+        }
     }
     
 }
