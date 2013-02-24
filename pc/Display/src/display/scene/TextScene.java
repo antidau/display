03e50340bcf14da21322132242af4bfccbc435bd
Jakob Wenzel <jakobwenzel92@gmail.com>
2013-02-24 16:24:32 +0100

    Slight optimizations in TextScene
    Instead of filling the whole Image with black and then drawing
    characters, now only otherwise unchanged parts are filled with black.

--- pc/Display/src/display/scene/TextScene.java
+++ pc/Display/src/display/scene/TextScene.java
@@ -28,7 +28,7 @@ public class TextScene extends Scene {
     @Override
     public void drawFrame(Image img, float delta) {
         deltasum+=delta;
-        img.fill(0);
+        
         if (deltasum>1f) { position--; deltasum-=1f; System.out.println("hello");}
         int relposition = position;
         int currentChar =0;
@@ -38,21 +38,28 @@ public class TextScene extends Scene {
             
             if (data!=null) {
                 
-                for (int x =0;x<5;x++) {
+                for (int x =0;x<6;x++) {
                     if (relposition+x>=Image.WIDTH) break;
                     if (relposition+x<0) continue;
                     for (int y=0;y<8;y++) {
-                        int p=(data[x] >> y) & 1;
+                        int p;
+                        //Last column is always empty
+                        if (x==5) p=0; 
+                        //Get right bit for current pixel
+                        else p=(data[x] >> y) & 1;
                         img.data[y][x+relposition]=p;
                     }
                     
                 }
+                //Clear space column
                 relposition+=6;
                 
             }
             currentChar++;
             
         }
+        //Fill Space right of text
+        img.fillRect(relposition, 0, Image.WIDTH, Image.HEIGHT, 0);
     }
     
 }
