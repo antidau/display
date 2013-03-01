6c48891a67fd66b7cd6c1b4a84cff46b0155cdb9
Jakob Wenzel <jakobwenzel92@gmail.com>
2013-03-01 16:48:06 +0100

    added buttons to add scene and view properties from web

--- pc/Display/src/display/editor/webPages/main.js
+++ pc/Display/src/display/editor/webPages/main.js
@@ -1,6 +1,7 @@
 $(function(){
         
     sceneList = $('#sceneList');
+    details = $('#details');
     loading = "loading";
 
     function loadSceneList() {
@@ -11,8 +12,7 @@ $(function(){
             success: function(msg) {
                 sceneList.html('');
                 for (i=0;i<msg.length;i++) {
-                    alert(msg[i]);
-                    sceneList.append('<div class="scene">'+msg[i]+'</div>');
+                    sceneList.append('<a href="javascript:loadProperties('+i+');">'+msg[i]+'</div>');
                 }
             },
             fail: function(err) {
@@ -22,4 +22,64 @@ $(function(){
         })
     }
     loadSceneList();
+    
+    loadProperties = function(id) {
+        details.html(loading);
+        $.ajax({
+            url: '/json/scene/properties?id='+id,
+            dataType: 'json',
+            success: function(msg) {
+                
+                lines = msg.reduce(function (l,prop) {
+                    return l+"<tr><td>"+prop.name+"</td><td>"+prop.content+"</td></tr>";
+                },'');
+                details.html("<table><tr><th>Name</th><th>Content</th></tr>"+lines+"</table>");
+                
+            },
+            fail: function(err) {
+                alert(err);
+            }
+        });
+    }
+    
+    add = function(type) {
+        
+        if (type) {
+            $.ajax({
+                url: '/json/scene/add?name='+type,
+                dataType: 'json',
+                success: function(msg) {
+
+                    if (msg) loadSceneList();
+                    else alert('Adding '+type+' failed.');
+                },
+                fail: function(err) {
+                    alert(err);
+                }
+            });
+        } else {
+            
+            details.html(loading);
+            $.ajax({
+                url: '/json/types',
+                dataType: 'json',
+                success: function(msg) {
+
+                    lines = msg.reduce(function (l,type) {
+                        return l+'<button onclick="add(\''+type+'\')">Add '+type+'</button>';
+                    },'');
+                    details.html(lines);
+
+                },
+                fail: function(err) {
+                    alert(err);
+                }
+            });
+            
+        }
+        
+    }
 });
+var loadProperties;
+var add;
+
